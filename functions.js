facb891719930e4f00d6c35590abf47e92438e73
NeilTheSeal <neil.hendren@gmail.com>
2025-03-19 09:52:55 -0600

    Add comments

--- functions.js
+++ functions.js
@@ -11,7 +11,10 @@ let g = {
 };
 
 let graphicsWrapper = document.getElementById("graphics-wrapper");
-let containerDims = [800, 600]
+
+// This is the size of the canvas. I set it to 800x600, but it could
+// be any arbitrary height and width.
+let containerDims = [800, 600];
 
 function setup() {
   // Create the p5.js canvas inside #graphics-wrapper
@@ -37,14 +40,22 @@ function draw() {
 
 /* Optionally add more complex math or animations here */
 
+// When the window is resized, this changes the canvas size
+// To match the size of the graphics-wrapper element.
 function windowResized() {
   resizeCanvas(graphicsWrapper.offsetWidth, graphicsWrapper.offsetHeight);
 }
 
+// This is used to calculate how large the canvas is compared to what
+// you state in containerDims. For example, if the canvas is 1600x1200 px
+// on the screen and canvasDims[0] is 800, then this will return 2. It is
+// used to scale the canvas and mouse coordinates correctly.
 function relativeSize() {
   return graphicsWrapper.offsetWidth / containerDims[0];
 }
 
+// This function scales the image to fit the graphics wrapper element.
+// Don't touch this.
 function handleScaling() {
   window.width = containerDims[0];
   window.height = containerDims[1];
@@ -53,6 +64,9 @@ function handleScaling() {
   scale(relativeSize());
 }
 
+// If you want to use mouseX and mouseY for whatever reason,
+// this function corrects for the fact that the canvas scales in size.
+// Don't touch this.
 function handleMouseScaling() {
   document.querySelector("canvas").addEventListener("mousemove", (e) => {
     let rect = graphicsWrapper.getBoundingClientRect();
@@ -70,6 +84,8 @@ function handleMouseScaling() {
 
 function handleCanvasSize() {
   graphicsWrapper.style.width = "calc(100vw - 40px)";
+  // When you remove the "Currently work in progress" element,
+  // change 190px to 135px in the two statements below.
   graphicsWrapper.style.maxWidth = `calc(calc(100vh - 190px) * ${containerDims[0]} / ${containerDims[1]})`;
   graphicsWrapper.style.height = `calc(calc(100vw - 40px) * ${containerDims[1]} / ${containerDims[0]})`;
   graphicsWrapper.style.maxHeight = `calc(100vh - 190px)`;
